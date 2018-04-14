07a71f0f99b984b79e36c524eaedde3a0fd80701
arvin855 <arvink855@gmail.com>
2018-04-14 12:15:18 +0000

    added messages

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,5 +1,21 @@
 var app = angular.module('chatApp', ['ngMaterial']);
 
 app.controller('chatController', function ($scope) {
-
+    $scope.messages = [
+        {
+            'sender': "USER",
+            'text': 'Hello'
+	},
+        {
+            'sender': "BOT",
+            'text': 'Hello there '
+	},
+        {
+            'sender': "USER",
+            'text': 'whats the time'
+	},
+        {
+            'sender': "BOT",
+            'text': 'see your watch'
+	}];
 });
