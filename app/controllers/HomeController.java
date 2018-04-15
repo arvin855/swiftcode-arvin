692fe8040d45c2e6d10badff2231788d3da0f699
arvin855 <arvink855@gmail.com>
2018-04-15 07:46:19 +0000

    added color_texture

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -21,4 +21,9 @@ public class HomeController extends Controller {
         return ok(chat.render());
     }
 
+    public LegacyWebSocket<String> chatSocket() {
+        return WebSocket.withActor(MessageActor::props);
+    }
+
 }
+
