ae5cadb0cc4a6730f0e73339db7189ab3eeefd79
arvin855 <arvink855@gmail.com>
2018-04-15 12:23:09 +0000

    final app completed

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -10,7 +10,7 @@ app.controller('chatController', function ($scope, $sce) {
     $scope.messages = [];
     $scope.trust = $sce.trustAsHtml;
 
-    var exampleSocket = new WebSocket('wss://https://swiftcode-arvin-news-chatbot.herokuapp.com/chatSocket');
+    var exampleSocket = new WebSocket('wss://swiftcode-arvin-news-chatbot.herokuapp.com/chatSocket');
 
     exampleSocket.onmessage = function (event) {
         var jsonData = JSON.parse(event.data);
